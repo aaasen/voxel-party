2a61b74f316a519b928979ea1cf6a8d88e12f8e3
Lane Aasen <laneaasen@gmail.com>
2012-11-28 12:03:23 -0800

    starts work on controller

--- main/Controller.java
+++ main/Controller.java
@@ -1,5 +1,64 @@
 package main;
 
-public class Controller {
+import org.lwjgl.LWJGLException;
+import org.lwjgl.input.Keyboard;
+import org.lwjgl.input.Mouse;
 
+public class Controller implements Runnable {
+	Thread inputThread;
+	
+	public Controller() {
+		this.inputThread = new Thread(this, "input_thread");
+		try {
+			Mouse.create();
+			Keyboard.create();
+		} catch (LWJGLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		this.inputThread.start();
+	}
+	
+	
+	@Override
+	public void run() {
+		pollInput();
+	}
+
+	public void pollInput() {		
+		if (Mouse.isButtonDown(0)) {
+			int x = Mouse.getX();
+			int y = Mouse.getY();
+
+			System.out.println("MOUSE DOWN @ X: " + x + " Y: " + y);
+		}
+
+		if (Keyboard.isKeyDown(Keyboard.KEY_SPACE)) {
+			System.out.println("SPACE KEY IS DOWN");
+		}
+
+		while (Keyboard.next()) {
+			if (Keyboard.getEventKeyState()) {
+				if (Keyboard.getEventKey() == Keyboard.KEY_A) {
+					System.out.println("A Key Pressed");
+				}
+				if (Keyboard.getEventKey() == Keyboard.KEY_S) {
+					System.out.println("S Key Pressed");
+				}
+				if (Keyboard.getEventKey() == Keyboard.KEY_D) {
+					System.out.println("D Key Pressed");
+				}
+			} else {
+				if (Keyboard.getEventKey() == Keyboard.KEY_A) {
+					System.out.println("A Key Released");
+				}
+				if (Keyboard.getEventKey() == Keyboard.KEY_S) {
+					System.out.println("S Key Released");
+				}
+				if (Keyboard.getEventKey() == Keyboard.KEY_D) {
+					System.out.println("D Key Released");
+				}
+			}
+		}
+	}
 }
