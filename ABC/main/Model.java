4863bc19167bf8a9aaf1c66a8f4d667c97a99140
Lane Aasen <laneaasen@gmail.com>
2012-11-28 11:52:33 -0800

    adds something that might be thread safety. maybe.

--- ABC/main/Model.java
+++ ABC/main/Model.java
@@ -3,13 +3,26 @@ package main;
 import java.util.ArrayList;
 
 public class Model {
+	boolean locked;
 	ArrayList<Player> players;
 	
 	public Model() {
 		this.players = new ArrayList<Player>();
 	}
 	
+	public void lock() {
+		this.locked = true;
+	}
+	
+	public void unlock() {
+		this.locked = false;
+	}
+	
 	public void addPlayer(Player player) {
+		this.lock();
+		
 		this.players.add(player);
+		
+		this.unlock();
 	}
 }
