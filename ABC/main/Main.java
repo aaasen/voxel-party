45a9be47052c4ad56d158bcafa40bbd67cd5f6ac
Lane Aasen <laneaasen@gmail.com>
2012-11-28 19:32:53 -0800

    sets lowest opengl version to 3.0

--- ABC/main/Main.java
+++ ABC/main/Main.java
@@ -5,15 +5,21 @@ import org.lwjgl.input.Keyboard;
 import org.lwjgl.input.Mouse;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.PixelFormat;
+import org.lwjgl.opengl.ContextAttribs;
 
 public class Main {
     public static void main(String[] argv) {
     	Model world = new Model();
     	
+    	PixelFormat pixelFormat = new PixelFormat();
+    	ContextAttribs contextAttributes = new ContextAttribs(3, 0);
+    	contextAttributes.withForwardCompatible(true);
+    	
         try {
-        	
         	Display.setDisplayMode(new DisplayMode(800, 600));
-        	Display.create();
+        	Display.create(pixelFormat, contextAttributes);
         	Keyboard.create();
         	Mouse.create();
 	    
