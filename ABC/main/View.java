f7ff064cd4a4d8c74b7b4535e7fa2705be7eb356
Lane Aasen <laneaasen@gmail.com>
2012-11-28 19:05:58 -0800

    splits the view into init(), run() and render(). this will make controlling fps easier

--- ABC/main/View.java
+++ ABC/main/View.java
@@ -11,35 +11,42 @@ public class View {
 	Model model;
 	int width, height;
 	boolean stop = false;
-	
+
 	public View(Model model, int width, int height) {
 		this.model = model;
 		this.width = width;
 		this.height = height;
 	}
+
+	public void init() {
+		glMatrixMode(GL_PROJECTION);
+		glLoadIdentity();
+		glOrtho(0, this.width, 0, this.height, 1, -1);
+		glMatrixMode(GL_MODELVIEW);
 	
-    public void run() {
-    	// init OpenGLs
-    	glMatrixMode(GL_PROJECTION);
-    	glLoadIdentity();
-    	glOrtho(0, this.width, 0, this.height, 1, -1);
-    	glMatrixMode(GL_MODELVIEW);
-     
-    	while (!Display.isCloseRequested() && !this.stop) {
-    		if(!model.locked) {
-    			// Clear the screen and depth buffer
-    			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);	
-
-    			model.player.draw();
-
-    			Display.update();
-    		}
-    	}
-     
-    	Display.destroy();
+		this.run();
 	}
-    
-    public void stop() {
-    	this.stop = true;
-    }
+
+	public void run() {
+		while (!Display.isCloseRequested() && !this.stop) {
+			this.render();
+		}
+
+		Display.destroy();
+	}
+
+	public void render() {
+		if(!model.locked) {
+			// Clear the screen and depth buffer
+			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);	
+
+			model.player.draw();
+
+			Display.update();
+		}
+	}
+
+	public void stop() {
+		this.stop = true;
+	}
 }
