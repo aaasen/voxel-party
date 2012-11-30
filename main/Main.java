94b2cb7be9771a06421c8b4794ba6431c0e7178e
Lane Aasen <laneaasen@gmail.com>
2012-11-29 17:47:44 -0800

    adds comments to point

--- main/Main.java
+++ main/Main.java
@@ -18,7 +18,6 @@ public class Main {
 	public static final int HEIGHT = 600;
 	
     public static void main(String[] argv) {
-    	
         try {
         	// initialize lwjgl
         	Display.setDisplayMode(new DisplayMode(WIDTH, HEIGHT));
--- main/Point.java
+++ main/Point.java
@@ -1,8 +1,22 @@
 package main;
 
+/**
+ * A generic 3D point made of floats for OpenGL compatibility
+ * 
+ * @author Lane Aasen <laneaasen@gmail.com>
+ *
+ */
+
 public class Point {
 	float x, y, z;
 	
+	/**
+	 * Constructs a Point
+	 * 
+	 * @param x
+	 * @param y
+	 * @param z
+	 */
 	public Point(float x, float y, float z) {
 		this.x = x;
 		this.y = y;
