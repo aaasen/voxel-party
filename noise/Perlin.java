402d3e5b22a7d82cd4cd800eca1e024d39e1c882
Lane Aasen <laneaasen@gmail.com>
2012-11-30 11:19:27 -0800

    basic perlin noise working

--- noise/Perlin.java
+++ noise/Perlin.java
@@ -9,5 +9,40 @@ package noise;
  */
 
 public class Perlin {
+	public static final float P = 0.5f;
+	public static final int OCTAVES = 4;
 	
+	/*
+	  function PerlinNoise_2D(float x, float y)
+
+      total = 0
+      p = persistence
+      n = Number_Of_Octaves - 1
+
+      loop i from 0 to n
+
+          frequency = 2i
+          amplitude = pi
+
+          total = total + InterpolatedNoisei(x * frequency, y * frequency) * amplitude
+
+      end of i loop
+
+      return total
+
+  end function
+  */
+	
+	public static float perlin2D(float x, float y) {
+		float total = 0.0f;
+		
+		for (int i = 0; i < OCTAVES; i++) {
+			int frequency = (int) Math.pow(2, i);
+			float amplitude = (float) Math.pow(P, i);
+			
+			total += Noise.interpolateNoise2f(x * frequency, y * frequency, Noise.primes0) * amplitude;
+		}
+		
+		return total;
+	}
 }
--- objects/Terrain.java
+++ objects/Terrain.java
@@ -11,6 +11,7 @@ import static org.lwjgl.opengl.GL11.glColor3f;
 import glhelper.Planes;
 import glhelper.Outlines;
 import main.Point;
+import noise.*;
 
 public class Terrain implements Renderable {
 	Point[][] matrix;
@@ -29,27 +30,16 @@ public class Terrain implements Renderable {
 	 * Populates the Terrain matrix
 	 */
 	public void genTerrain() {
-//		for (int i = 0; i < matrix.length; i++) {
-//			for (int j = 0; j < matrix[i].length; j++) {
-//				matrix[i][j] = new Point(i - matrix.length / 2, 0, j - matrix[i].length / 2);
-//			}
-//		}
-//		
-//		for (int i = 8; i < matrix.length - 8; i += 16) {
-//			for (int j = 8; j < matrix[i].length - 8; j += 16) {
-//				matrix[i][j].y += hashNoise(512 * i + j);
-//			}
-//		}
-//		
-//		for (int i = 0; i < matrix.length - 16; i++) {
-//			for (int j = 0; j < matrix[i].length - 16; j++) {
-//				matrix[i][j].y += (float) matrix[i + (i % 16 + 8)][j + (j % 16 + 8)].y + hashNoise(512 * i + j);
-//			}
-//		}
-		
 		for (int i = 0; i < matrix.length; i++) {
 			for (int j = 0; j < matrix[i].length; j++) {
-				matrix[i][j] = new Point(i - matrix.length / 2, 1 + ((float) Math.random() * 1.0f), j - matrix[i].length / 2);
+				matrix[i][j] = new Point(i - matrix.length / 2, 0, j - matrix[i].length / 2);
+			}
+		}
+
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < matrix[i].length; j++) {
+				matrix[i][j].y = Math.abs(Perlin.perlin2D(matrix[i][j].x, matrix[i][j].z));
+				System.out.println(matrix[i][j].y);
 			}
 		}
 	}
