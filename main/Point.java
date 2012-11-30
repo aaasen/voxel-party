6f10d9dcd55573c396a7dcb79c391fa9684e2c37
Lane Aasen <laneaasen@gmail.com>
2012-11-30 09:18:54 -0800

    changes camera to a more fps like style. only w and s are currently hooked up

--- main/Point.java
+++ main/Point.java
@@ -22,4 +22,19 @@ public class Point {
 		this.y = y;
 		this.z = z;
 	}
+	
+	/**
+	 * Adds two Points
+	 * 
+	 * @param a
+	 * @param b
+	 * @return sum of a and b
+	 */
+	public static Point add(Point a, Point b) {
+		return new Point(a.x + b.x, a.y + b.y, a.z + b.z);
+	}
+	
+	public static Point scale(Point a, float scalar) {
+		return new Point(a.x * scalar, a.y * scalar, a.z * scalar); 
+	}
 }
