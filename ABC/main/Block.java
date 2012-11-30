a0cc948228f1850da803be88cbf3c4347d2d5a06
Lane Aasen <laneaasen@gmail.com>
2012-11-29 18:24:30 -0800

    adds comments and placeholders to block

--- ABC/main/Block.java
+++ ABC/main/Block.java
@@ -1,6 +1,18 @@
 package main;
 
+/**
+ * A generic cube
+ * 
+ * @author Lane Aasen <laneaasen@gmail.com>
+ *
+ */
+
 public class Block implements Renderable {
+	Point position;
+	
+	public Block(Point position) {
+		this.position = position;
+	}
 	
 	@Override
 	public void draw() {
