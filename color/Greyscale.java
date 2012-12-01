c46fed76bd04a3ae1afb082c283ac19b02b95ce2
Lane Aasen <laneaasen@gmail.com>
2012-11-30 17:48:43 -0800

    rewrote colorists in a sensical, object oriented way

---
+++ color/Greyscale.java
@@ -0,0 +1,19 @@
+package color;
+
+import static org.lwjgl.opengl.GL11.glColor3f;
+
+public class Greyscale extends Colorist {
+	public Greyscale(float range, float offset) {
+		super(range, offset);
+	}
+	
+	/**
+	 * Selects a color based on the value
+	 * 
+	 * @param value
+	 */
+	public void color(float value) {
+		value = this.value(value);
+		glColor3f(value, value, value);
+	}
+}
