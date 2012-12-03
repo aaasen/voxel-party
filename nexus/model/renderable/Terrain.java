ff1630f9eaf69072fa4395c5dfdfb3effa3592a5
Lane Aasen <laneaasen@gmail.com>
2012-12-03 10:41:53 -0800

    removes water and biomes.

--- nexus/model/renderable/Terrain.java
+++ nexus/model/renderable/Terrain.java
@@ -12,20 +12,19 @@ import static nexus.model.structs.Chunk.CHUNK_DIMENSION;
 import java.util.ArrayList;
 
 import nexus.model.generators.Perlin;
-import nexus.model.structs.Biome;
 import nexus.model.structs.Vector3;
+import nexus.view.color.Greyscale;
 
 public class Terrain implements Renderable {
 	public static final int BIG_NUMBER = (int) Math.pow(2, 18);
 	
 	ArrayList<Block> blocks;
-	Biome biome;
 	float xDilation, zDilation;
 	int x, z;
 	int width, depth;
 	float y;
 	
-	public Terrain(int x, int z, float y, int width, int depth, float xDilation, float zDilation, Biome biome) {
+	public Terrain(int x, int z, float y, int width, int depth, float xDilation, float zDilation) {
 		this.x = x;
 		this.z = z;
 		this.y = y;
@@ -34,7 +33,6 @@ public class Terrain implements Renderable {
 		this.blocks = new ArrayList<Block>();
 		this.xDilation = xDilation;
 		this.zDilation = zDilation;
-		this.biome = biome;
 		
 		genTerrain();
 	}
@@ -47,13 +45,13 @@ public class Terrain implements Renderable {
 			for (int j = 0; j < depth + 1; j++) {
 				float x = (this.x * CHUNK_DIMENSION + i);
 				float z = (this.z * CHUNK_DIMENSION + j);
-				float y = this.biome.yDilation * ((1.0f + Perlin.perlin2D(x * this.xDilation + BIG_NUMBER, z * this.zDilation + BIG_NUMBER)) / 2.0f);
+				float y = ((1.0f + Perlin.perlin2D(x * this.xDilation + BIG_NUMBER, z * this.zDilation + BIG_NUMBER)) / 2.0f);
 				
 //				System.out.println(y);
 				
 				for (float k = 0.0f; k < y; k += 1.0f) {
 //					System.out.println(k);
-					this.blocks.add(new Block(new Vector3(x, k, z), new Vector3(x + 1, k + 1, z + 1), this.biome.colorist));
+					this.blocks.add(new Block(new Vector3(x, k, z), new Vector3(x + 1, k + 1, z + 1), new Greyscale(20.0f, 0.0f)));
 				}
 			}
 		}
--- nexus/model/structs/Chunk.java
+++ nexus/model/structs/Chunk.java
@@ -8,25 +8,18 @@ package nexus.model.structs;
  */
 
 import nexus.model.renderable.Terrain;
-import nexus.model.renderable.Water;
-import nexus.view.color.Greyscale;
 
 public class Chunk {
 	// this value should not need to be changed
 	public static final int CHUNK_DIMENSION = 16;
 	
 	public Terrain terrain;
-	public Water water;
+	
 	int x, y;
 	
 	public Chunk(int x, int y) {
 		this.x = x;
 		this.y = y;
-		this.terrain = new Terrain(x, y, 0.0f, CHUNK_DIMENSION, CHUNK_DIMENSION,
-				0.1f, 0.1f,
-				new Biome(20.0f, new Greyscale(20.0f, 0.0f)));
-//		this.water = new Water(new Vector3(x * CHUNK_DIMENSION, 0.0f, y * CHUNK_DIMENSION),
-//				CHUNK_DIMENSION, CHUNK_DIMENSION, 
-//				new Color(0.55f, 0.804f, 0.97f));
+		this.terrain = new Terrain(x, y, 0.0f, CHUNK_DIMENSION, CHUNK_DIMENSION, 0.1f, 0.1f);
 	}
 }
