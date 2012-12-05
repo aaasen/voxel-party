cc0943e04faf2f86a66f1951085a3fe953f1e594
Lane Aasen <laneaasen@gmail.com>
2012-12-04 22:22:58 -0800

    makes the terrain a little more interesting

--- nexus/main/Main.java
+++ nexus/main/Main.java
@@ -17,11 +17,11 @@ import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
 
 public class Main {
-	public static final int WIDTH = 800;
-	public static final int HEIGHT = 600;
+	public static final int WIDTH = 1600;
+	public static final int HEIGHT = 900;
 	public static final String WINDOW_TITLE = "Nexus";
-	public static final int INIT_RENDER_DISTANCE = 5;
-	public static final Vector3 INIT_CAMERA_POSITION = new Vector3(0.0f, 8.0f, 0.0f);
+	public static final int INIT_RENDER_DISTANCE = 4;
+	public static final Vector3 INIT_CAMERA_POSITION = new Vector3(0.0f, 32.0f, 0.0f);
 	public static final float INIT_SENSITIVITY = 0.3f;
 	public static final boolean CAMERA_INVERT = true;
 	
