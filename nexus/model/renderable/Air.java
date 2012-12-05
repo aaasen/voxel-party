47cd7394f93bb86e82a31d7cf33b2fe3206d8ddb
Lane Aasen <laneaasen@gmail.com>
2012-12-04 17:10:07 -0800

    closes #13 (multiple block types)

---
+++ nexus/model/renderable/Air.java
@@ -0,0 +1,15 @@
+package nexus.model.renderable;
+
+import nexus.model.structs.Vector3;
+
+public class Air extends Block {
+
+	public Air(Vector3 a, float dimension) {
+		super(a, dimension, null);
+	}
+	
+	@Override
+	public void draw() {
+		
+	}
+}
