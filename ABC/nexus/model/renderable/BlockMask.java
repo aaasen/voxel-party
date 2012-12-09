ad2eed7c60ab6a8a21d7c0ff493301b6c0b002a6
Lane Aasen <laneaasen@gmail.com>
2012-12-08 21:06:23 -0800

    adds outlining capabilities to block mask

--- ABC/nexus/model/renderable/BlockMask.java
+++ ABC/nexus/model/renderable/BlockMask.java
@@ -4,13 +4,20 @@ package nexus.model.renderable;
  * BlockMasks define which parts of a Block must be rendered
  */
 
+import static org.lwjgl.opengl.GL11.glColor3f;
+import static org.lwjgl.opengl.GL11.glLineWidth;
 import nexus.model.structs.Block;
+import nexus.view.gl.Outlines;
 import nexus.view.gl.Planes;
 
 public class BlockMask implements Renderable {
-
+	public static final float OUTLINE_R = 0.5f;
+	public static final float OUTLINE_G = 0.5f;
+	public static final float OUTLINE_B = 0.5f;
+	public static final float OUTLINE_WIDTH = 4f;
+	
 	public Block block;
-	public boolean render, top, bottom, near, far, left, right;
+	public boolean render, top, bottom, near, far, left, right, outline;
 	
 	public BlockMask(Block block) {
 		this.block = block;
@@ -21,6 +28,7 @@ public class BlockMask implements Renderable {
 		far = false;
 		left = false;
 		right = false;
+		outline = false;
 	}
 	
 	/**
@@ -41,6 +49,10 @@ public class BlockMask implements Renderable {
 				Planes.drawQuad2f(block.a.x, block.a.y, block.a.z, block.a.x, block.b.y, block.b.z, block.colorist);	
 			} if (right) {
 				Planes.drawQuad2f(block.b.x, block.a.y, block.a.z, block.b.x, block.b.y, block.b.z, block.colorist);	
+			} if (outline) {
+				glColor3f(OUTLINE_R, OUTLINE_G, OUTLINE_B);
+				glLineWidth(OUTLINE_WIDTH);
+				Outlines.rectPrism2f(block.a, block.b);
 			}
 		}
 	}
