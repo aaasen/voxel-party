a2832b38ebbdfdbb1fe925f18cb45492f8ad2c4b
Lane Aasen <laneaasen@gmail.com>
2013-01-08 10:55:57 -0800

    adds some more Vector utilities from Dylan Swiggett

--- nexus/model/structs/Vector3.java
+++ nexus/model/structs/Vector3.java
@@ -4,6 +4,7 @@ package nexus.model.structs;
  * A generic 3D point made of floats for OpenGL compatibility
  * 
  * @author Lane Aasen <laneaasen@gmail.com>
+ * @auhtor Dylan Swiggett <dylanswiggett@gmail.com>
  *
  */
 
@@ -22,27 +23,53 @@ public class Vector3 {
 		this.y = y;
 		this.z = z;
 	}
-	
-	/**
-	 * Adds two Points
-	 * 
-	 * @param a
-	 * @param b
-	 * @return sum of a and b
-	 */
+
 	public Vector3 add(Vector3 a) {
 		return new Vector3(a.x + x, a.y + y, a.z + z);
 	}
 	
 	public Vector3 subtract(Vector3 a) {
-		return new Vector3(x - a.x, y - a.y, z - a.z);
+		return new Vector3(a.x - x, a.y - y, a.z - z);
 	}
+	
+	public float length() {
+        return (float) Math.sqrt(x * x + y * y + z * z);
+    }
+
+    public float lengthSquared() {
+        return x * x + y * y + z * z;
+    }
 	
 	public Vector3 scale(float scalar) {
 		return new Vector3(x * scalar, y * scalar, z * scalar); 
 	}
 	
+    public Vector3 normalize() {
+        return scale(1 / length());
+    }
+
+    public float dotProduct(Vector3 vector) {
+        return x * vector.x + y * vector.y + z * vector.z;
+    }
+
+    public Vector3 crossProduct(Vector3 vector) {
+        return new Vector3(y * vector.z - z * vector.y, z * vector.x - x * vector.z, x * vector.y - y * vector.x);
+    }
+
+    public Vector3 projection(Vector3 vector) {
+        float length = vector.length();
+        return vector.scale(dotProduct(vector) / (length * length));
+    }
+
+    public Vector3 perpendicular(Vector3 vector) {
+        return subtract(projection(vector));
+    }
+	
 	public String toString() {
 		return "(" + x + ", " + y + ", " + z + ")";
+	}
+	
+	public boolean equals(Vector3 a) {
+		return this.x == a.x && this.y == a.y && this.z == a.z;
 	}
 }
