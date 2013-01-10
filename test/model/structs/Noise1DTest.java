ab428df3b91a85ca6c22d7e8a76984e3874df7d8
Lane Aasen <laneaasen@gmail.com>
2013-01-10 08:44:27 -0800

    adds distribution and bounds unit tests to noise1D. closes #18

--- test/model/structs/Noise1DTest.java
+++ test/model/structs/Noise1DTest.java
@@ -2,8 +2,11 @@ package model.structs;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.Collection;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
+import java.util.TreeSet;
 
 import nexus.model.generators.Noise1D;
 
@@ -11,20 +14,45 @@ import org.junit.Test;
 
 public class Noise1DTest {
 	public static final int TEST_ITERATIONS = (int) Math.pow(2, 20);
+	public final TreeSet<Float> noise = new TreeSet<Float>(genRandom(TEST_ITERATIONS));
 	
 	@Test
 	public void noiseCollision() {
-		Set<Float> results = new HashSet<Float>();
-		
-		for (int i = 0; i < TEST_ITERATIONS; i++) {
-			results.add(Noise1D.noise(i, Noise1D.primes[i % 3]));
-		}
-		
-		assertEquals(TEST_ITERATIONS - results.size() + " collisions (" + (float) results.size() / TEST_ITERATIONS + "%)", TEST_ITERATIONS, results.size());
+		assertEquals(TEST_ITERATIONS - noise.size() + " collisions (" + (float) noise.size() / TEST_ITERATIONS + "%)", TEST_ITERATIONS, noise.size());
 	}
 	
 	@Test
 	public void noiseDistribution() {
+		float expectedAverage = (noise.first() + noise.last()) / 2;
+		double sum = 0;
+		Iterator<Float> iter = noise.iterator();
 		
+		while (iter.hasNext()) {
+			sum += iter.next();
+		}
+		
+		float realAverage = (float) (sum / noise.size());
+		
+		assertEquals(expectedAverage, realAverage, 0.1f);
+	}
+	
+	@Test
+	public void noiseLow() {
+		assertEquals(true, noise.first() >= 0);
+	}
+	
+	@Test
+	public void noiseHigh() {
+		assertEquals(true, noise.last() <= 0);
+	}
+	
+	private Set<Float> genRandom(int iterations) {
+		Set<Float> results = new HashSet<Float>();
+		
+		for (int i = 0; i < iterations; i++) {
+			results.add(Noise1D.noise(i, Noise1D.primes[i % 3]));
+		}
+		
+		return results;
 	}
 }
