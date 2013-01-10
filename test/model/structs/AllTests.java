82a7b8ea8397a0c1f428be652dab75c44dea18c8
Lane Aasen <laneaasen@gmail.com>
2013-01-09 17:34:33 -0800

    adds test suite for all available tests

---
+++ test/model/structs/AllTests.java
@@ -0,0 +1,11 @@
+package model.structs;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ ChunkContainerTest.class, Vector3Test.class })
+public class AllTests {
+
+}
