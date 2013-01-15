9b8359a7ee59c76817ac77a98d5932cce5ce9701
Lane Aasen <laneaasen@gmail.com>
2013-01-15 10:25:18 -0800

    adds argument checks to makeQuad2f()

--- src/nexus/view/gl/Planes.java
+++ src/nexus/view/gl/Planes.java
@@ -142,6 +142,10 @@ public class Planes {
 	 * @return float[] that, when parsed as triangles, will for a quad
 	 */
 	public static float[] makeQuad2f(Vector3 a, Vector3 b) {
-		return Vector3.combine(new Vector3[] { a, b, new Vector3(a.x, b.y, a.z), b, a, new Vector3(b.x, a.y, b.z)});
+		if (a.x <= b.x && a.y <= b.y && a.z <= b.z) {
+			return Vector3.combine(new Vector3[] { a, b, new Vector3(a.x, b.y, a.z), b, a, new Vector3(b.x, a.y, a.z)});
+		} else {
+			throw new IllegalArgumentException("all values of a must be smaller than those of b");
+		}
 	}
 }
